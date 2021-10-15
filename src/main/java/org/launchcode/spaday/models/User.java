2b93e277f168fa7469c218b67424431706c5f36b
DarrenFritts <84247704+DarrenFritts@users.noreply.github.com>
2021-10-14 20:18:56 -0500

    user-signup-pt2 complete

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,8 +1,20 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.*;
+
 public class User {
+    @NotNull(message="username cannot be null")
+    @NotBlank(message="username cannot be blank")
+    @NotEmpty(message = "username cannot be empty")
+    @Size(min = 5, max = 15, message = "username needs to be between 5-15 characters")
     private String username;
+
+    @NotNull(message="email cannot be null")
+    @Email
     private String email;
+
+    @NotNull(message="password cannot be null")
+    @Size(min = 6, message = "password must be at least 6 characters")
     private String password;
 
     public User() {
